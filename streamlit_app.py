eba22c0850bec0bcd3c3f56161475cc740cea1a3
AinurK-PhD <ainur.khakimov2023@gmail.com>
2025-04-06 18:32:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+from st_pages import Page, show_pages, add_page_title
 
 st.set_page_config(page_title="NurSim", page_icon="🏠", layout="wide")
+
+# Adds this page’s title to the sidebar
+add_page_title()
+
+# Show custom sidebar labels/icons
+show_pages(
+    [
+        Page("streamlit_app.py", "Home", "🏠"),
+        Page("pages/1_Input_Data.py", "Input Data", "🧾"),
+        # You can add more pages here later
+    ]
+)
 
 st.title("NurSim: A Basic Reservoir Simulation App")
 
