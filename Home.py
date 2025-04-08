27b9dd3bedb9ef2fbfd88d8fa27000da0faf5492
AinurK-PhD <ainur.khakimov2023@gmail.com>
2025-04-08 17:40:33 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,23 +5,20 @@ st.set_page_config(page_title="NurSim", page_icon="🏠", layout="wide")
 st.title("NurSim: A Basic Reservoir Simulation App")
 
 st.info("""
-**Welcome to NurSim** – a simple 2D reservoir simulator built using Finite Difference Methods (FDM).
+Welcome to **NurSim** – a 2D Interactive Reservoir Simulator based on the **IMPES method** (Implicit Pressure, Explicit Saturation) for two-phase (oil-water) flow.
 
-🔍 **Features**:
+🔍 **Features:**
 - Customizable 2D grid for reservoir modeling
 - Add injectors and producers interactively
-- Simulate single-phase pressure distribution over time
-- Visualize pressure fields and well effects
+- Simulate two-phase flow using IMPES (pressure & saturation fields)
+- Visualize pressure and saturation maps with well markers
 
-🛠️ **Built with**:
+🛠️ **Built with:**
 - Python 🐍
 - Streamlit 🌐
 - NumPy & Matplotlib 📊
 
-🧠 **Created by:** Ainur Khakimov (PhD Candidate, Department of Petroleum Engineering)
-
-👀 **Supervised by:** Dr. Mohammad Sarim Jamal (Instructor, Department of Petroleum Engineering)
-
-
+🧠 **Created by:** Ainur Khakimov (PhD Candidate, Department of Petroleum Engineering)  
+👨‍🏫 **Supervised by:** Dr. Mohammad Sarim Jamal (Instructor, Department of Petroleum Engineering)  
 📁 [GitHub Repository](https://github.com/AinurK-PhD/NurSim)
 """)
