33b71179383bf815aa9c272033c5f78b1b4d030d
AinurK-PhD <ainur.khakimov2023@gmail.com>
2025-04-16 16:27:04 +0300

    Adding success display for Reset -  1_Input_Data.py

--- pages/1_Input_Data.py
+++ pages/1_Input_Data.py
@@ -6,6 +6,13 @@ import streamlit as st
 st.title("🧾 Input Data – NurSim Setup")
 st.markdown("Define all parameters for the reservoir simulation. All units are in **field units**.")
 
+
+if st.session_state.get("reset_triggered", False):
+    st.session_state["reset_triggered"] = False
+    st.rerun()
+
+
+
 # -----------------------------
 # Default values
 # -----------------------------
@@ -103,5 +110,6 @@ with col_reset:
     if st.button("🔄 Reset to Default"):
         for key, val in defaults.items():
             st.session_state[key] = val
+        st.session_state["reset_triggered"] = True
         st.success("All fields reset to default values.")
-        st.rerun()
+
